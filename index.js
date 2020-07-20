8b679169ab3d2e8cc2de28f8b633cf82afc4ae38
sisouza <sidia.reis1@gmail.com>
2020-07-20 17:23:25 -0400

    'adicionando partials e enviando dados do form '

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
+const bodyParser  = require("body-parser")
 
 //utilizando ejs como view enginer
 app.set('view engine','ejs');
 app.use(express.static('public'));
-   
+
+//capturando os dados do form com bodyparser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//rotas
 app.get("/",(req, res)=> {
     res.render("index");
 });
@@ -12,5 +18,12 @@ app.get("/",(req, res)=> {
 app.get("/perguntar",(req, res)=> {
     res.render("perguntar");
 });
+
+app.post("/salvarpergunta",(req, res)=> {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;  
+    res.send("Formulario enviado com sucesso" + "titulo:" + titulo + "descricao:"+descricao);
+});
+
 
 app.listen(8080,() => {console.log("App executando...");});
